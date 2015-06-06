886ac83698fdcbcdc6a8edf52e8f93b67f7e9851
Muhammad Farag <MuhammadFarag@Gmail.com>
2015-06-06 18:47:48 +0200

    Replaced the deprecated ActionBarActivity by AppCompatActivity

--- app/src/main/java/com/muhammadfarag/portfolio/MyAppPortfolio.java
+++ app/src/main/java/com/muhammadfarag/portfolio/MyAppPortfolio.java
@@ -3,6 +3,7 @@ package com.muhammadfarag.portfolio;
 import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,7 +11,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 
-public class MyAppPortfolio extends ActionBarActivity {
+public class MyAppPortfolio extends AppCompatActivity {
 
     public void launchApp(View view) {
         Button button = (Button) view;
