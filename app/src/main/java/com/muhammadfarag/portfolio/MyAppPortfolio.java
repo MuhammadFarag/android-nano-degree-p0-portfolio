091e9d95429becb262c643e2c0fc7ab194872f51
Muhammad Farag <MuhammadFarag@Gmail.com>
2015-06-10 21:04:07 +0200

    removed unused imports

--- app/src/main/java/com/muhammadfarag/portfolio/MyAppPortfolio.java
+++ app/src/main/java/com/muhammadfarag/portfolio/MyAppPortfolio.java
@@ -1,10 +1,8 @@
 package com.muhammadfarag.portfolio;
 
 import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
